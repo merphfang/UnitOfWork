e34c1bb3d6bb725f8bd1013d085e807fd27ded9d
merphfang <merphfang@yahoo.com>
2018-02-09 14:14:11 -0500

    Add DB Project

---
+++ DB/Script/Table_Customer.sql
@@ -0,0 +1,25 @@
+﻿/****** Object:  Table [dbo].[Customer]    Script Date: 2/9/2018 2:12:52 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Customer](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](200) NOT NULL,
+	[Address1] [nvarchar](200) NOT NULL,
+	[Address2] [nvarchar](200) NULL,
+	[City] [nvarchar](50) NULL,
+	[Province] [nvarchar](50) NULL,
+	[Country] [nvarchar](50) NULL CONSTRAINT [DF_Customer_Country]  DEFAULT (N'CANADA'),
+ CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
---
+++ DB/Script/Table_User.sql
@@ -0,0 +1,28 @@
+﻿/****** Object:  Table [dbo].[User]    Script Date: 2/9/2018 2:13:10 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Email] [nvarchar](200) NOT NULL,
+	[FirstName] [nvarchar](100) NOT NULL,
+	[LastName] [nvarchar](100) NOT NULL,
+	[Password] [nvarchar](max) NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[Roles] [nvarchar](200) NOT NULL,
+	[UserId] [uniqueidentifier] NOT NULL,
+	[IsActive] [bit] NOT NULL CONSTRAINT [DF_User_IsActive]  DEFAULT ((1)),
+	[CreatedDate] [datetime] NOT NULL CONSTRAINT [DF_User_CreatedDate]  DEFAULT (getdate()),
+	[UpdatedDate] [datetime] NOT NULL CONSTRAINT [DF_User_UpdatedDate]  DEFAULT (getdate()),
+ CONSTRAINT [UN_User_Email] PRIMARY KEY CLUSTERED 
+(
+	[Email] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+
